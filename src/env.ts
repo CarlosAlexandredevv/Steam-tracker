652f474765c58436dde51f1ac4b7b85fef46fead
carlosalexandredevv <alexfilho062005@gmail.com>
2026-01-20 18:49:48 -0300

    Add better-auth package and update Prisma schema with User, Session, Account, and Verification models

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 
 const clientEnvSchema = z.object({
   NEXT_PUBLIC_API_URL: z.url().optional().default('http://localhost:3000'),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.url(),
   DATABASE_URL: z.url(),
 });
 
-export const clientEnv = clientEnvSchema.parse(process.env);
+export const env = clientEnvSchema.parse(process.env);
