7dabaeef608ca13d8865277ac916863618e08bf3
Carlos Alexandre <143346006+CarlosAlexandredevv@users.noreply.github.com>
2026-01-23 18:31:43 -0300

    Atualizar o env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,13 +2,12 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const clientEnvSchema = z.object({
-  NEXT_PUBLIC_API_URL: z.string().url().optional().default('http://localhost:3000'),
   JWT_SECRET: z.string().optional(),
   DATABASE_URL: z.url().optional(),
   GITHUB_CLIENT_ID: z.string().optional(),
   GITHUB_CLIENT_SECRET: z.string().optional(),
   STEAM_API_KEY: z.string().optional(),
-  APP_URL: z.url().optional(),
+  NEXT_PUBLIC_APP_URL: z.url().optional(),
 });
 
 export const env = clientEnvSchema.parse(process.env);
