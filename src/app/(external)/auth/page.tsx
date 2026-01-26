ba4b2649a129ed08921abe698f70987ddd671478
carlosalexandredevv <alexfilho062005@gmail.com>
2026-01-26 11:54:48 -0300

    Update package dependencies, enhance global styles with custom themes, and improve authentication UI with new button components.

--- src/app/(external)/auth/page.tsx
+++ src/app/(external)/auth/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   function handleLogin() {
     window.location.href = "/api/auth/steam";
   }
   return (
     <main>
-      <button onClick={handleLogin}>Login with Steam</button>
+      <Button variant={"default"} onClick={handleLogin}>Login with Steam</Button>
+      <Button variant={"secondary"} onClick={handleLogin}>Login with Steam</Button>
+      <Button variant={"destructive"} onClick={handleLogin}>Login with Steam</Button>
+      <Button variant={"outline"} onClick={handleLogin}>Login with Steam</Button>
+      <Button variant={"ghost"} onClick={handleLogin}>Login with Steam</Button>
     </main>
   );
 }
