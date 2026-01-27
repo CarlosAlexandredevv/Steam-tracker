a4c7384d8d4df97e2c7f79f3820ec053728de0d0
carlosalexandredevv <alexfilho062005@gmail.com>
2026-01-27 10:22:34 -0300

    Refactor SearchPlayer component to improve player search functionality by using a debounced input handler. Replace state management for steamId with a more efficient useRef approach to prevent unnecessary re-renders.

--- src/components/header/search-player.tsx
+++ src/components/header/search-player.tsx
@@ -1,63 +1,66 @@
-"use client";
+'use client';
 
-import {  useState, useEffect } from "react";
-import { InputGroup, InputGroupInput, InputGroupAddon } from "../ui/input-group";
-import { Search } from "lucide-react";
-import { getPlayerById } from "@/app/actions/user/get-player-by-id";
-import { SteamPlayer } from "@/types/steam";
+import { useRef, useState } from 'react';
 import {
-  Popover,
-  PopoverAnchor,
-} from "@/components/ui/popover"
-import { PlayerPopoverContent } from "./player-popover-content";
+  InputGroup,
+  InputGroupInput,
+  InputGroupAddon,
+} from '../ui/input-group';
+import { Search } from 'lucide-react';
+import { getPlayerById } from '@/app/actions/user/get-player-by-id';
+import { SteamPlayer } from '@/types/steam';
+import { Popover, PopoverAnchor } from '@/components/ui/popover';
+import { PlayerPopoverContent } from './player-popover-content';
 
 export function SearchPlayer() {
-const [steamId, setSteamId] = useState("");
-const [player, setPlayer] = useState<SteamPlayer | null>(null);
-const [open, setOpen] = useState(false);
-const [notFound, setNotFound] = useState(false);
+  const [player, setPlayer] = useState<SteamPlayer | null>(null);
+  const [open, setOpen] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-useEffect(() => {
-    const timeoutId = setTimeout(async () => {
-        if(!steamId.trim()) {
-            setOpen(false);
-            setNotFound(false);
-            return;
-        }
-        
-        const searchPlayer = await getPlayerById(steamId);
-        if(searchPlayer) {
-            setPlayer(searchPlayer);
-            setNotFound(false);
-            setOpen(true);
-        } else {
-            setPlayer(null);
-            setNotFound(true);
-            setOpen(true);
-        }
+  function handleSearchPlayer(value: string) {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (!value.trim()) {
+      setOpen(false);
+      setNotFound(false);
+      setPlayer(null);
+      return;
+    }
+
+    timeoutRef.current = setTimeout(async () => {
+      const searchPlayer = await getPlayerById(value);
+
+      if (searchPlayer) {
+        setPlayer(searchPlayer);
+        setNotFound(false);
+        setOpen(true);
+      } else {
+        setPlayer(null);
+        setNotFound(true);
+        setOpen(true);
+      }
     }, 500);
+  }
 
-    return () => clearTimeout(timeoutId);
-}, [steamId]);
-
-return (
-  <div className="flex flex-col gap-2">
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverAnchor asChild>
-      <InputGroup className="w-full lg:max-w-96">
-          <InputGroupInput 
-            placeholder="Steam ID"  
-            value={steamId}
-            onChange={(e) => setSteamId(e.target.value)} 
-          />
-          <InputGroupAddon>
-            <Search />
-          </InputGroupAddon>
-        </InputGroup>
-      </PopoverAnchor>
-      <PlayerPopoverContent player={player} notFound={notFound} />
-    </Popover>
-  </div>
-
+  return (
+    <div className="flex flex-col gap-2">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverAnchor asChild>
+          <InputGroup className="w-full lg:max-w-96">
+            <InputGroupInput
+              placeholder="Steam ID"
+              onChange={(e) => handleSearchPlayer(e.target.value)}
+            />
+            <InputGroupAddon>
+              <Search />
+            </InputGroupAddon>
+          </InputGroup>
+        </PopoverAnchor>
+        <PlayerPopoverContent player={player} notFound={notFound} />
+      </Popover>
+    </div>
   );
 }
